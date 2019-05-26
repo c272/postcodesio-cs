07fae54c530f3789e8d2ca8798358788eb8416fb
C272 <realrussianmanconfirmed@gmail.com>
2019-05-26 17:32:43 +0100

    Add basic API fetcher, testing.

---
+++ PostcodeAPIFetcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PostcodeIO.API
+{
+    public class PostcodeAPIFetcher
+    {
+        public static PostcodeResult GetPostcodeInfo(string postcode)
+        {
+            //Replace any spaces in the postcode with "%20" for URL encoding.
+            postcode = postcode.Replace(" ", "%20");
+
+            //Grabbing the JSON string from API.
+            string contents;
+            using (var wc = new System.Net.WebClient())
+            {
+                contents = wc.DownloadString("http://api.postcodes.io/postcodes/" + postcode);
+            }
+
+            //Serializing using Newtonsoft.JSON to a PostcodeInfo object.
+            var postcodeInfo = JsonConvert.DeserializeObject<PostcodeInfo>(contents);
+
+            //Checking for errors.
+            if (postcodeInfo.status != 200)
+            {
+                throw new Exception(postcodeInfo.error);
+            }
+
+            //Returning.
+            return postcodeInfo.result;
+        }
+    }
+}
---
+++ PostcodeInfo.cs
@@ -0,0 +1,60 @@
+﻿namespace PostcodeIO.API
+{
+    //The full postcode info for a requested postcode.
+    public class PostcodeInfo
+    {
+        //The HTTP status response from Postcodes.IO.
+        public int status;
+
+        //If an error is returned, it is held here.
+        public string error;
+
+        //The actual result of the API call.
+        public PostcodeResult result;
+    }
+
+    public class PostcodeResult
+    {
+        //The returned postcode.
+        public string postcode;
+
+        //The quality of the postcode.
+        public int quality;
+
+        //Eastings, northings.
+        public int eastings, northings;
+
+        //Country the postcode is in, region.
+        public string country;
+        public string region;
+
+        //The NHS domain of the postcode, primary care trust.
+        public string nhs_ha;
+        public string primary_care_trust;
+
+        //Longitude, latitude.
+        public double longitude;
+        public double latitude;
+
+        //The european electoral region of the postcode.
+        public string european_electoral_region;
+
+        //LSOA, MSOA
+        public string LSOA, MSOA;
+
+        //Postcode incode/outcode (region and subregion).
+        public string incode, outcode;
+
+        //Parliamentary/council stuff.
+        public string parliamentary_constituency, admin_district, parish, admin_ward, admin_county, ced, ccg, nuts;
+
+        //Codes for this postcode.
+        PostcodeCodes codes;
+    }
+
+    //The set of assigned codes for this postcode.
+    public class PostcodeCodes
+    {
+        public string admin_district, admin_county, admin_ward, parish, parliamentary_constituency, ccg, ced, nuts;
+    }
+}
